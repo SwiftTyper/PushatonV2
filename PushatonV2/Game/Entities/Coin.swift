d57e50900e15c4f6797184684454648117379d8c
SwiftTyper <theswifttyper@gmail.com>
2025-01-04 12:12:14 +0100

    few small bug fixes

--- PushatonV2/Game/Entities/Coin.swift
+++ PushatonV2/Game/Entities/Coin.swift
@@ -9,7 +9,7 @@ import Foundation
 import SceneKit
 
 class Coin: SCNNode {
-    static let radius: CGFloat = 0.3
+    static let radius: CGFloat = 0.4
     
     override init() {
         super.init()
@@ -23,7 +23,7 @@ class Coin: SCNNode {
     }
     
     private func setupCoin(radius: CGFloat) {
-        let coinGeometry = SCNCylinder(radius: radius, height: 0.05)
+        let coinGeometry = SCNCylinder(radius: radius, height: 0.08)
         
         let material = SCNMaterial()
         material.diffuse.contents = UIColor(red: 1.0, green: 0.8, blue: 0.0, alpha: 1.0)
@@ -58,7 +58,7 @@ class Coin: SCNNode {
     
     private func startSpinAnimation() {
         let rotationAnimation = SCNAction.repeatForever(
-            SCNAction.rotateBy(x: 0, y: CGFloat.pi * 2, z: 0, duration: 1.5)
+            SCNAction.rotateBy(x: 0, y: CGFloat.pi * 2, z: 0, duration: 3)
         )
         self.runAction(rotationAnimation)
     }
@@ -74,36 +74,38 @@ extension Coin {
         let spacing: Float = 3.0
         
         switch type {
-        case .straight:
-            let length = Bool.random() ? 6 : 4
-            for i in 1...length {
-                let coin = Coin()
-                let z = obstacle.position.z + Float(i) * spacing
-                coin.position = SCNVector3(obstacle.position.x, baseY, z)
-                
-                let moveAction = SCNAction.moveBy(x: 0, y: 0, z: 20, duration: 1)
-                coin.runAction(SCNAction.repeatForever(moveAction))
-                
-                scene.rootNode.addChildNode(coin)
-            }
-        case .doubleStraight:
-            for j in 0..<2 {
-                for i in 1...6 {
+            case .straight:
+                let length = Bool.random() ? 6 : 4
+                for i in 1...length {
                     let coin = Coin()
-                    let z = obstacle.position.z + Float(i) * spacing
-                    let y = baseY + Float(j) * 1.0
-                    coin.position = SCNVector3(obstacle.position.x, y, z)
+                    let z = obstacle.position.z + Float(i) * spacing + spacing
+                    coin.position = SCNVector3(obstacle.position.x, baseY, z)
                     
                     let moveAction = SCNAction.moveBy(x: 0, y: 0, z: 20, duration: 1)
                     coin.runAction(SCNAction.repeatForever(moveAction))
                     
                     scene.rootNode.addChildNode(coin)
                 }
-            }
+            case .doubleStraight:
+                for j in 0..<2 {
+                    for i in 1...6 {
+                        let coin = Coin()
+                        let z = obstacle.position.z + Float(i) * spacing + spacing
+                        let y = baseY + Float(j) * 1.0
+                        coin.position = SCNVector3(obstacle.position.x, y, z)
+                        
+                        let moveAction = SCNAction.moveBy(x: 0, y: 0, z: 20, duration: 1)
+                        coin.runAction(SCNAction.repeatForever(moveAction))
+                        
+                        scene.rootNode.addChildNode(coin)
+                    }
+                }
             case .arc:
-                let radius: Float = 2.5
-                for i in 0..<5 {
-                    let angle = Float(i) * (.pi / 4)
+                let radius: Float = 2
+                let numberOfCoins: Int = 5
+                for i in 0..<numberOfCoins {
+                    let progress = Float(i) / Float(numberOfCoins - 1)
+                    let angle = .pi * progress
                     let z = obstacle.position.z + radius * cos(angle)
                     let y = baseY + radius * sin(angle)
                     let coin = Coin()
@@ -114,7 +116,7 @@ extension Coin {
                     
                     scene.rootNode.addChildNode(coin)
                 }
-            }
+        }
     }
     
     static func update(_ gameController: GameController) {
